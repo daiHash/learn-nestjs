c09bcd02011be50d6d957c94da3e06e83af8086e
daiHash <dhashi92@gmail.com>
2020-12-01 02:36:00 +0900

    Add validation pipe for create task

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query, UsePipes, ValidationPipe } from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFiltereDto } from './dto/getTasksfiltered.dto';
 import { Task } from './tasks.model';
@@ -22,6 +22,7 @@ export class TasksController {
   }
 
   @Post()
+  @UsePipes(ValidationPipe)
   createTask(@Body() createTaskDto: CreateTaskDto): Task {
     return this.tasksService.createTask(createTaskDto)
   }
