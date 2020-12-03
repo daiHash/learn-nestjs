ffcd51383f6767081e1a3bbc5995116d25d4f9e1
daiHash <dhashi92@gmail.com>
2020-12-04 02:46:30 +0900

    Validate task not found

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFiltereDto } from './dto/getTasksfiltered.dto';
 import { Task } from './tasks.model';
@@ -21,12 +21,16 @@ export class TasksService {
     if (search) {
       tasks = tasks.filter(task => task.title.includes(search) || task.description.includes(search))
     }
-    
+
     return tasks
   }
 
   getTaskById(id: string): Task {
-    return this.tasks.find((task) => task.id === id)
+    const task =  this.tasks.find((task) => task.id === id)
+    if (!task) {
+      throw new NotFoundException(`Task with id: ${id} does not exist`)
+    }
+    return task
   }
 
   createTask(createTaskDto: CreateTaskDto): Task {
