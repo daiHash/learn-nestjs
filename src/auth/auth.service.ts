fcb9f3a5c4ba98d12472e36c24e325603c8f668c
daiHash <dhashi92@gmail.com>
2020-12-11 01:25:41 +0900

    Validate user in signin

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, UnauthorizedException } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
 import { AuthCredentialsDTO } from './dto/auth-credentials.dto'
 import { UserRepository } from './user.repository'
@@ -9,7 +9,17 @@ export class AuthService {
     @InjectRepository(UserRepository) private userRepository: UserRepository
   ) {}
 
-  signUp(authCredentialsDto: AuthCredentialsDTO) {
+  async signUp(authCredentialsDto: AuthCredentialsDTO): Promise<string> {
     return this.userRepository.signUp(authCredentialsDto)
+  }
+
+  async signIn(authCredentialsDto: AuthCredentialsDTO) {
+    const username = await this.userRepository.validateUserPassword(
+      authCredentialsDto
+    )
+
+    if (!username) {
+      throw new UnauthorizedException('Invalid credentials')
+    }
   }
 }
