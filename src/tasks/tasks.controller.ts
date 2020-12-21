38705e555fea15479504f9927037ee342480c3ac
daiHash <dhashi92@gmail.com>
2020-12-22 02:45:22 +0900

    Authguard taks

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,21 +1,36 @@
-import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, Query, UsePipes, ValidationPipe } from '@nestjs/common';
-import { CreateTaskDto } from './dto/create-task.dto';
-import { GetTasksFiltereDto } from './dto/getTasksfiltered.dto';
-import { TaskStatusValidationPipe } from './dto/pipes/taskStatusValidation.pipe';
-import { Task } from './task.entity';
-import { TasksService } from './tasks.service';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  Query,
+  UseGuards,
+  UsePipes,
+  ValidationPipe
+} from '@nestjs/common'
+import { AuthGuard } from '@nestjs/passport'
+import { CreateTaskDto } from './dto/create-task.dto'
+import { GetTasksFiltereDto } from './dto/getTasksfiltered.dto'
+import { TaskStatusValidationPipe } from './dto/pipes/taskStatusValidation.pipe'
+import { Task } from './task.entity'
+import { TasksService } from './tasks.service'
 
 @Controller('tasks')
+@UseGuards(AuthGuard())
 export class TasksController {
   constructor(private tasksService: TasksService) {}
 
   @Get()
-  getTasks(@Query(ValidationPipe) filterDto: GetTasksFiltereDto){
+  getTasks(@Query(ValidationPipe) filterDto: GetTasksFiltereDto) {
     return this.tasksService.getAllTasks(filterDto)
   }
 
   @Get('/:id')
-  getTaskById(@Param('id',ParseIntPipe) id: number): Promise<Task> {
+  getTaskById(@Param('id', ParseIntPipe) id: number): Promise<Task> {
     return this.tasksService.getTaskById(id)
   }
 
